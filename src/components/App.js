36717cd367ed6206f0f8a04b026c4692292e4369
chadwyck242 <chadwyck.codeart@gmail.com>
2018-12-30 16:53:09 -0700

    Refactor onSearchSubmit function into arrow function, keep async/await functions

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   state = { images: [] };
 
   // Used ES8 Async/Await functions here
-  async onSearchSubmit(term) {
+  // Refactored to an arrow function to avoid 'this' issue
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -15,9 +16,9 @@ class App extends React.Component {
           "Client-ID ea16d2d3f22d5aa6438dda7d0696dc1b97e1d169f4e5d49aa667712ef46416cf"
       }
     });
-    
+
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
