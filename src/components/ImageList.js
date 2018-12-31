0149fadaa2493a0ab8adba4a99eb81748914ddaf
chadwyck242 <chadwyck.codeart@gmail.com>
2018-12-31 15:37:34 -0700

    Add map method to create list of images, with key id and object destructuring

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const ImageList = (props) => {
-  console.log(props.images);
+const ImageList = props => {
+  // use the map method to create a list of image elements
+  // this list is assigned to images variable which is
+  // referenced in the div below
+  const images = props.images.map(({ description, id, urls }) => {
+    // notice the use of destructuring...
+    return <img key={id} src={urls.regular} alt={description} />;
+  });
 
-  return <div>ImageList</div>
+  return <div>{images}</div>;
 };
 
 export default ImageList;
