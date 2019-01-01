74226e74e42136c97a1a400edbe7ade5791ff6d3
chadwyck242 <chadwyck.codeart@gmail.com>
2019-01-01 15:27:41 -0700

    Create ImageList.css file and add CSS Grid Layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,16 @@
-import React from "react";
+import './ImageList.css';
+import React from 'react';
 
 const ImageList = props => {
   // use the map method to create a list of image elements
   // this list is assigned to images variable which is
   // referenced in the div below
   const images = props.images.map(({ description, id, urls }) => {
-    // notice the use of destructuring...
+    // notice the use of destructuring above and below...
     return <img key={id} src={urls.regular} alt={description} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
