f7338feec5bfa2c327f8782f791d734dbe7c3113
chadwyck242 <chadwyck.codeart@gmail.com>
2019-01-06 19:40:28 -0700

    Finalize Pics project, add dynamic CSS Grid styling to optimally calculate image layout

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -6,6 +6,9 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    // setting spans state
+    this.state = { spans: 0 }
+
     // how to create a reference (ref) in React
     this.imageRef = React.createRef();
   }
@@ -19,7 +22,14 @@ class ImageCard extends React.Component {
   // this function will calculate how many grid rows the image will span
   // so we can dynamically set the grid-row-span CSS property for each image
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    // capture the height retunred by ref in const height
+    const height = this.imageRef.current.clientHeight;
+
+    // calculate the number of spans based on image height / grid row height
+    const spans = Math.ceil(height / 10);
+
+    // set spans as state, using ES2015 syntax to shorten declaration
+    this.setState( { spans } );
   }
 
   render() {
@@ -27,7 +37,8 @@ class ImageCard extends React.Component {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      // inline styling to add the grid-row-end CSS with spans value
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
